8586a46bdd8ca2b986b2522f489775126ae9dff4
yangdigi <yangdigi@gmail.com>
2025-11-15 12:45:33 +0800

    dev

---
+++ docs/src/data/footnotes.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: CC-BY-NC-SA-4.0
+ */
+
+import example from "@site/docs/keymaps/_footnotes/example.mdx";
+import iosApplication from "@site/docs/keymaps/_footnotes/ios-application.mdx";
+import iosPower from "@site/docs/keymaps/_footnotes/ios-power.mdx";
+import macosEditing from "@site/docs/keymaps/_footnotes/macos-editing.mdx";
+import macosPower from "@site/docs/keymaps/_footnotes/macos-power.mdx";
+import macosUndoRedo from "@site/docs/keymaps/_footnotes/macos-undo-redo.mdx";
+import globe from "@site/docs/keymaps/_footnotes/globe.mdx";
+
+export default {
+  example,
+  iosApplication,
+  iosPower,
+  macosEditing,
+  macosPower,
+  macosUndoRedo,
+  globe,
+};
